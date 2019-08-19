ca39041183a51657d044ebf38b80b4b1d7ea544f
shevqnov <sshevqnov@yandex.ru>
2019-08-19 16:53:53 +0500

    ci

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,19 +5,23 @@ interface Props extends CardType { }
 
 const Card: React.FC<Props> = (props) => {
   const [isUpsideDown, setIsUpsideDown] = React.useState<boolean>(false)
+  const [checked, setChecked] = React.useState<boolean>(false)
   return (
     <div style={{
       padding: 10,
       margin: 15,
-      backgroundColor: '#ccc',
+      backgroundColor: checked && !isUpsideDown ? 'green' : '#ccc',
       width: 300,
       height: 150,
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       flexDirection: 'column',
-      borderRadius: 5
-    }} onClick={() => setIsUpsideDown(!isUpsideDown)}>
+      borderRadius: 5,
+    }} onClick={() => {
+      setChecked(true)
+      setIsUpsideDown(!isUpsideDown)
+    }}>
       {isUpsideDown ? (
         <React.Fragment>
           <p>{props.meaning}</p>
